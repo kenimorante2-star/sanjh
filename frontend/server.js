5a10e1497c81dd6fdc396214aa0d1b996374b500
Ken Morante <kenimorante@gmail.com>
2025-10-09 13:58:15 +0800

    fix

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config(); // loads only your local .env
 
 const app = express();
-const PORT = import.meta.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000;
 
 // Serve static files from build
 app.use(express.static(path.resolve("./dist")));
